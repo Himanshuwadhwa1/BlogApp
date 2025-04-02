d2030cfc527e91b54630783280d588a49c1caeb0
Himanshu wadhwa <himanshuhw999@gmail.com>
2025-04-02 14:51:18 +0530

    updated loader in index page

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
 import Post from "../Post";
 import { url } from "../utils/url";
+import {OrbitProgress} from "react-loading-indicators"
 
 export default function IndexPage(){
     const [posts,setPosts] = useState([]);
+    const [isloading,setIsloading] = useState(true);
     const url2 = url;
     useEffect(()=>{
         fetch(`${url2}/post`).then(response=>{
             response.json().then(post=>{
+                if(post){
+                    setIsloading(false);
+                }
                 setPosts(post);
             })
         })
     },[])
     return(
         <>
-            {posts.length>0 && posts.map((post)=>{
+        {isloading && (
+            <div className="loading">
+                <OrbitProgress variant="dotted" color="#888" size="large" text="" textColor="" />
+            </div>
+        ) }
+            {!isloading && posts.length>0 && posts.map((post)=>{
                 return <Post {...post} />;
             })}
         </>
